fc5dfb1758d1518ce9a99d3a8f2086acc6c56798
3epochs <1738547868@qq.com>
2019-03-02 16:31:55 +0800

    finish proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -2,65 +2,40 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDequeGold {
-    public static Integer expected;
-    public static Integer actual;
-
     @Test
     public void testArrayDeque() {
         ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        String message = "\n";
 
-/*        for (int i= 0; i < 10; i +=1 ) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (numberBetweenZeroAndOne < 0.5) {
-                solution.addFirst(i);
-                student.addFirst(i);
-            } else {
-                solution.addLast(i);
-                student.addLast(i);
-            }
-        }
-
-        for (int i = 0; i < 10; i += 1) {
-            if (! solution.isEmpty()) {
-                double numberBetweenZeroAndOne = StdRandom.uniform();
-                if (numberBetweenZeroAndOne < 0.5) {
-                    expected = solution.removeFirst();
-                    actual = student.removeFirst();
-                } else {
-                    expected = solution.removeLast();
-                    actual = student.removeLast();
-                }
-                assertEquals(expected + " not equal to " + actual, expected, actual);
-            }
-        }*/
-
-        for (int i = 0; i < 10; i += 1) {
+        for (int i = 0; i < 100; i += 1) {
             double rand = StdRandom.uniform();
             if(rand < 0.25) {
                 solution.addFirst(i);
                 student.addFirst(i);
+                message += "addFirst(" + i + ")\n";
             } else if (0.25 <= rand && rand < 0.5) {
                 solution.addLast(i);
                 student.addLast(i);
+                message += "addLast(" + i + ")\n";
             } else if (0.5 <= rand && rand < 0.75) {
                 if (!solution.isEmpty()) {
-                    expected = solution.removeFirst();
-                    actual = student.removeFirst();
-                    assertEquals(expected, actual);
+                    Integer expected = solution.removeFirst();
+                    Integer actual = student.removeFirst();
+                    message += "removeFirst()\n";
+                    assertEquals(message, expected, actual);
                 }
             } else {
                 if (!solution.isEmpty()) {
-                    expected = solution.removeLast();
-                    actual = student.removeLast();
-                    assertEquals(expected, actual);
+                    Integer expected = solution.removeLast();
+                    Integer actual = student.removeLast();
+                    message += "removeLast()\n";
+                    assertEquals(message, expected, actual);
                 }
             }
+            System.out.println(message);
         }
     }
-/*        for (int i = 0; i < solution.size(); i += 1) {
-            assertEquals(solution.get(i), student.get(i));
-        }*/
 }
 
 
