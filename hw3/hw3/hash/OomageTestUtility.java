9aa1d650fe31c3bf6cf6a93f0e9f1f4941093aff
3epochs <1738547868@qq.com>
2019-03-12 11:44:26 +0800

    finish hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -12,6 +12,15 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] scores = new int[M];
+        for (Oomage oomage : oomages) {
+            int index = (oomage.hashCode() & 0x7FFFFFFF) % M;
+            scores[index] += 1;
+        }
+        for (int i = 0; i < M; i += 1) {
+            if (scores[i] < oomages.size() / 50) return false;
+            if (scores[i] > oomages.size() / 2.5) return false;
+        }
+        return true;
     }
 }
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -36,15 +36,20 @@ public class TestComplexOomage {
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
-
-        // Your code here.
+        for (int i = 1; i < 50; i += 1) {
+            ArrayList<Integer> params = new ArrayList<>();
+            for (int j = 0; j < i; j +=1 ) {
+                params.add(1);
+            }
+            ComplexOomage coo = new ComplexOomage(params);
+            deadlyList.add(coo);
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
